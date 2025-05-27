8c4c3b2c65b223ce1dda4ce88c4f5ec3ab4b8519
root <root@DESKTOP-KVH5ITH.>
2025-05-27 09:43:21 +0300

    Ignore frontend

--- frontend/src/components/common/TableComponent.jsx
+++ frontend/src/components/common/TableComponent.jsx
@@ -23,6 +23,7 @@ const AnimatedRow = ({
   initial={{ opacity: 0, y: 10 }}
   animate={{ opacity: 1, y: 0 }}
   transition={{ duration: 0.3, ease: 'easeOut' }}
+    onClick={() => onRowClick?.(row)}
 >
       {onView && can('view') && (
         <td className="p-2 text-center">
