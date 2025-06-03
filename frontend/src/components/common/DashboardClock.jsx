3a299a3feaaedec780243b95db05ff82246e4aeb
root <root@DESKTOP-KVH5ITH.localdomain>
2025-06-03 09:16:08 +0300

    Add DashboardClock.jsx component

--- frontend/src/components/common/DashboardClock.jsx
+++ frontend/src/components/common/DashboardClock.jsx
@@ -23,26 +23,25 @@ export default function DashboardClock() {
   };
 
   return (
-    <div className="relative w-[140px] h-[140px] md:w-[120px] md:h-[120px] mx-auto mt-4 rounded-full shadow-lg border-[2px] border-greenic-dark bg-royal-light dark:bg-navy-darker bg-gradient-to-t from-gold/40 via-royal/10 to-gold/40  dark:from-navy-darker/30 dark:via-royal-dark/50 dark:to-navy-darker/40 transition-all duration-300">
-      
-      {/* مركز الساعة */}
+    <div className="relative w-32 h-32 md:w-36 md:h-36 mx-auto mt-4 rounded-full shadow-lg border-2 border-greenic-dark bg-royal-light dark:bg-navy-darker bg-gradient-to-t from-gold/40 via-royal/10 to-gold/40 dark:from-navy-darker/30 dark:via-royal-dark/50 dark:to-navy-darker/40 transition-all duration-300">
+      {/* Clock center */}
       <div className="absolute w-3 h-3 bg-black dark:bg-white rounded-full top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10" />
 
-      {/* عقارب الساعة */}
+      {/* Clock hands */}
       <div
-        className="absolute top-1/2 left-1/2 w-[4px] h-[35px] bg-red-600 rounded-full origin-bottom z-10"
+        className="absolute top-1/2 left-1/2 w-1 h-9 bg-red-600 rounded-full origin-bottom z-10"
         style={{ transform: `translate(-50%, -100%) rotate(${hourDeg}deg)` }}
       />
       <div
-        className="absolute top-1/2 left-1/2 w-[3px] h-[45px] bg-black dark:bg-white rounded-full origin-bottom z-10"
+        className="absolute top-1/2 left-1/2 w-1 h-12 bg-black dark:bg-white rounded-full origin-bottom z-10"
         style={{ transform: `translate(-50%, -100%) rotate(${minDeg}deg)` }}
       />
       <div
-        className="absolute top-1/2 left-1/2 w-[2px] h-[55px] bg-green-700 dark:bg-green-300 rounded-full origin-bottom z-10"
+        className="absolute top-1/2 left-1/2 w-0.5 h-14 bg-green-700 dark:bg-green-300 rounded-full origin-bottom z-10"
         style={{ transform: `translate(-50%, -100%) rotate(${secDeg}deg)` }}
       />
 
-      {/* الأرقام العربية */}
+      {/* Arabic numerals */}
       {[...Array(12)].map((_, i) => {
         const angle = (i + 1) * 30;
         const radius = 40;
@@ -51,11 +50,11 @@ export default function DashboardClock() {
         return (
           <div
             key={i}
-            className="absolute text-[12px] md:text-[14px] font-bold text-navy dark:text-white"
+            className="absolute text-xs md:text-sm font-bold text-navy dark:text-white"
             style={{
               top: `${y}%`,
               left: `${x}%`,
-              transform: "translate(-100%, -50%)",
+              transform: "translate(-50%, -50%)",
             }}
           >
             {arabicNumbers[i + 1]}
@@ -65,3 +64,4 @@ export default function DashboardClock() {
     </div>
   );
 }
+ 
